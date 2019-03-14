35dd7fcd7e09362d8ff574ac752dfd5e99ade30f
Cesc <cesc.mateu@gmail.com>
2019-03-14 18:18:14 +0100

    Updating files Feb19

--- train_launcher.cmd
+++ train_launcher.cmd
@@ -1,15 +1,15 @@
 #!/bin/bash
-# @ job_name = architecture_HPAI
+# @ job_name = arq_cesc_complexa
 # @ initialdir = /gpfs/scratch/bsc28/bsc28487/tfm_cesc
-# @ output= logs/architecture_HPAI/%j.out
-# @ error= logs/architecture_HPAI/%j.err
+# @ output= logs/arq_cesc_complexa/%j.out
+# @ error= logs/arq_cesc_complexa/%j.err
 # @ total_tasks= 1 
-# @ gpus_per_node= 1	
-# @ cpus_per_task= 1
+# @ gpus_per_node= 4
+# @ cpus_per_task= 4
 # @ features= k80
 # @ wall_clock_limit= 04:30:00
 
 module purge
 module load K80/default impi/2018.1 mkl/2018.1 cuda/8.0 CUDNN/7.0.3 python/3.6.3_ML
 
-python train.py --model_dir experiments/architecture_HPAI --data_dir data/retina_TFRecords_small --preproc_file data/mean_channels_img.npy
+python train.py --model_dir experiments/arq_cesc_complexa --data_dir data/retina_TFRecords_small --preproc_file data/mean_channels_img.npy
